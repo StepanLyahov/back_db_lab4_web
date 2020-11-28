27562dce8fbf3bfc0c2f4b7c8b2eb1ca08baa2b8
Ляхов Степан <lyakhov.sr@reliab.tech>
2020-11-29 01:47:09 +0300

    feat: add void ListOfTaskMapper

---
+++ src/main/java/com/stepan/web/mapper/ListOfTaskMapper.java
@@ -0,0 +1,13 @@
+package com.stepan.web.mapper;
+
+import com.stepan.web.entity.ListOfTask;
+import com.stepan.web.web.dto.ListOfTaskDto;
+import org.mapstruct.Mapper;
+
+@Mapper
+public interface ListOfTaskMapper {
+
+//    ListOfTaskDto toDTO(ListOfTask entity);
+//
+//    ListOfTask toEntity(ListOfTaskDto dto);
+}
