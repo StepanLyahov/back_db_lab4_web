b7d901226e6ba784c370830e99c4653a9bad345e
Ляхов Степан <lyakhov.sr@reliab.tech>
2020-11-29 12:53:44 +0300

    feat: add realisation for ListOfTaskMapper

--- src/main/java/com/stepan/web/mapper/ListOfTaskMapper.java
+++ src/main/java/com/stepan/web/mapper/ListOfTaskMapper.java
@@ -3,11 +3,17 @@ package com.stepan.web.mapper;
 import com.stepan.web.entity.ListOfTask;
 import com.stepan.web.web.dto.ListOfTaskDto;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper
 public interface ListOfTaskMapper {
 
-//    ListOfTaskDto toDTO(ListOfTask entity);
-//
-//    ListOfTask toEntity(ListOfTaskDto dto);
+    @Mapping(target = "color", ignore = true)
+    @Mapping(target = "tasks", ignore = true)
+    ListOfTaskDto toDTO(ListOfTask entity);
+
+
+    @Mapping(target = "colorId", ignore = true)
+    @Mapping(target = "tasks", ignore = true)
+    ListOfTask toEntity(ListOfTaskDto dto);
 }
