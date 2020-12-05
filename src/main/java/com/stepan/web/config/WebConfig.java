fe757d090b3642f0cc02dc0273242687afec7d47
Ляхов Степан <lyakhov.sr@reliab.tech>
2020-12-05 20:04:31 +0300

    fix: fix problem with CROSS too

---
+++ src/main/java/com/stepan/web/config/WebConfig.java
@@ -0,0 +1,18 @@
+package com.stepan.web.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowCredentials(true)
+                .allowedHeaders("Cache-Control", "Content-Type")
+                .allowedMethods("HEAD", "POST")
+                .allowedOrigins("*");
+    }
+}
