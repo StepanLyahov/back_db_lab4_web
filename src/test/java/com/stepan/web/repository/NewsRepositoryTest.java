a9330c58ba3eb5a099f65a15a713550903633870
Ляхов Степан <lyakhov.sr@reliab.tech>
2020-12-27 21:52:56 +0300

    feat: add test for test news and add image in db

--- src/test/java/com/stepan/web/repository/NewsRepositoryTest.java
+++ src/test/java/com/stepan/web/repository/NewsRepositoryTest.java
@@ -1,10 +1,12 @@
 package com.stepan.web.repository;
 
+import com.stepan.web.entity.Image;
 import com.stepan.web.entity.News;
-import org.aspectj.lang.annotation.Before;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -18,16 +20,40 @@ class NewsRepositoryTest extends BaseTest {
     void testAddAndReadNewsFromDB() {
         newsRepository.deleteAll();
 
+        Image imageCat = getImage("Yor path to image in resources");
+
         News news = News.builder()
                 .description("My descr")
                 .lessDescription("less descr")
                 .title("My title")
+                .image(imageCat)
                 .build();
 
         newsRepository.save(news);
 
         List<News> res = newsRepository.findAll();
         assertEquals("My descr", res.get(0).getDescription());
+    }
+
+    private Image getImage(String path) {
+        File file = new File(path);
+        byte[] bFile = new byte[(int) file.length()];
+
+        try {
+            FileInputStream fileInputStream = new FileInputStream(file);
+            fileInputStream.read(bFile);
+            fileInputStream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        Image image = Image.builder()
+                .name("myImage")
+                .type("jpg")
+                .imageData(bFile)
+                .build();
+
+        return image;
     }
 
 }
