c6ec4164b16e515b9454b078da8661f10b4cda78
Ляхов Степан <lyakhov.sr@reliab.tech>
2020-12-27 21:20:37 +0300

    feat: ignore Image on mapping

--- src/main/java/com/stepan/web/mapper/InfoMapper.java
+++ src/main/java/com/stepan/web/mapper/InfoMapper.java
@@ -3,9 +3,11 @@ package com.stepan.web.mapper;
 import com.stepan.web.entity.Info;
 import com.stepan.web.web.v1.dto.InfoDto;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper
 public interface InfoMapper {
+    @Mapping(target = "logo", ignore = true)
     InfoDto toDto(Info info);
 
     Info toEntity(InfoDto dto);
--- src/main/java/com/stepan/web/mapper/NewsMapper.java
+++ src/main/java/com/stepan/web/mapper/NewsMapper.java
@@ -3,10 +3,12 @@ package com.stepan.web.mapper;
 import com.stepan.web.entity.News;
 import com.stepan.web.web.v1.dto.NewsDto;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper
 public interface NewsMapper {
 
+    @Mapping(target = "image", ignore = true)
     NewsDto toDto(News news);
 
     News toEntity(NewsDto dto);
