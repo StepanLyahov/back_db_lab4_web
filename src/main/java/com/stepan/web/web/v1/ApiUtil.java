8a143dd96958f7c545fa1e8c8b777b625fc6be6f
Ляхов Степан <lyakhov.sr@reliab.tech>
2020-12-27 20:04:52 +0300

    feat: add NewsController

---
+++ src/main/java/com/stepan/web/web/v1/ApiUtil.java
@@ -0,0 +1,11 @@
+package com.stepan.web.web.v1;
+
+public class ApiUtil {
+
+    private static final String ROOT = "api/v1";
+
+    public static final String NEWS = ROOT + "/news";
+
+    public static final String INFO = ROOT + "/info";
+
+}
---
+++ src/main/java/com/stepan/web/web/v1/controller/NewsController.java
@@ -0,0 +1,30 @@
+package com.stepan.web.web.v1.controller;
+
+import com.stepan.web.service.NewsService;
+import com.stepan.web.web.v1.dto.NewsDto;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+import static com.stepan.web.web.v1.ApiUtil.NEWS;
+
+@RestController
+@RequestMapping(NEWS)
+@RequiredArgsConstructor
+@CrossOrigin
+public class NewsController {
+
+    private final NewsService newsService;
+
+    @GetMapping("/getAllNews")
+    public List<NewsDto> getAllNews() {
+        return newsService.getAllNews();
+    }
+
+    @GetMapping("/getNews/{id}")
+    public NewsDto getNewsById(@PathVariable("id") Long id) {
+        return newsService.getNewsById(id);
+    }
+}
