818dbba1919eb539301fb6e7260a0789ff69496c
Ляхов Степан <lyakhov.sr@reliab.tech>
2021-01-06 20:46:57 +0300

    feat: add ifo test for fill in

--- src/test/java/com/stepan/web/repository/InfoRepositoryTest.java
+++ src/test/java/com/stepan/web/repository/InfoRepositoryTest.java
@@ -1,24 +1,57 @@
 package com.stepan.web.repository;
 
+import com.stepan.web.entity.Image;
 import com.stepan.web.entity.Info;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.io.File;
+import java.io.FileInputStream;
 
-class InfoRepositoryTest {
+
+class InfoRepositoryTest extends BaseTest {
 
     private InfoRepository infoRepository;
 
+    @Autowired
+    private ImageRepository imageRepository;
+
     @Test
-    @Disabled
-    void addInfo() {
+    void addAllInfoToDB() {
+
+        infoRepository.deleteAll();
+
+        // тебе нужна только одна info
         Info info = Info.builder()
                 .description("descr")
                 .logo(null)
+                .logoFacebook(addImage("","" ,""))
                 .number("number")
                 .build();
 
         infoRepository.save(info);
+
+    }
+
+    private Image addImage(String path, String nameImg, String typeImg) {
+        File file = new File(path);
+        byte[] bFile = new byte[(int) file.length()];
+
+        try {
+            FileInputStream fileInputStream = new FileInputStream(file);
+            fileInputStream.read(bFile);
+            fileInputStream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        Image image = Image.builder()
+                .name(nameImg) // имя картинки
+                .type(typeImg) // тип
+                .imageData(bFile)
+                .build();
+
+        return imageRepository.save(image);
     }
 }
